022eb5fd8386e448abdb9cd9838ddcd626d4ae7b
NourCodes <nouralhamwi2018@gmail.com>
2023-12-25 16:28:18 +0300

    New item page added.

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shoplist/pages/new_item.dart';
 import 'package:shoplist/widgets/grocery_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Groceries"),
+        actions: [
+          IconButton(
+            onPressed: addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: const GroceryList(),
     );
---
+++ lib/pages/new_item.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:shoplist/data/categories_db.dart';
+import 'package:shoplist/models/category_model.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({Key? key}) : super(key: key);
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+  var name = '';
+  var quantity = 1;
+  var selectedCategory = categories[AllCategories.fruit]!;
+  void saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add a New Item"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 30,
+                decoration: const InputDecoration(
+                  label: Text("Name"),
+                ),
+                onSaved: (newValue) {
+                  name = newValue!;
+                },
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 30) {
+                    return "Must be between 1 and 30 characters long";
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        label: Text("Quantity"),
+                      ),
+                      onSaved: (newValue) {
+                        quantity = int.parse(newValue!);
+                      },
+                      initialValue: quantity.toString(),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.parse(value) <= 0) {
+                          return "Must be a positive, valid number";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                        value: selectedCategory,
+                        items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                              value: category.value,
+                              child: Row(
+                                children: [
+                                  SizedBox.square(
+                                    dimension: 20,
+                                    child: ColoredBox(
+                                      color: category.value.color,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(category.value.title)
+                                ],
+                              ),
+                            ),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            selectedCategory = value!;
+                          });
+                        }),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text("Reset"),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: saveItem,
+                    child: const Text("Save"),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
