b19add53ad7e511445e6cc5901e78008e894cd04
NourCodes <nouralhamwi2018@gmail.com>
2023-12-28 20:16:20 +0300

    Retrieved and displayed data from Firebase Realtime Database

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shoplist/data/categories_db.dart';
 import 'package:shoplist/models/item_model.dart';
 import 'package:shoplist/pages/new_item.dart';
 import 'package:shoplist/widgets/grocery_list.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,21 +15,54 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Items> itemsList = [];
+  @override
+  void initState() {
+    loadItems();
+    super.initState();
+  }
+
+  //fetch data from firebase and load it
+  void loadItems() async {
+    final url = Uri.https(
+      'fluttershoplist-46e37-default-rtdb.firebaseio.com',
+      'shop-list.json',
+    );
+    // fetching data from the URL
+    final response = await http.get(url);
+    // decoding the response body, which is in JSON format, into a map
+    final Map<String, dynamic> dataList = json.decode(response.body);
+    // create a temporary list to store Items
+    final List<Items> templist = [];
+    // convert the map entries to Items and add them to the temporary list
+    for (final i in dataList.entries) {
+      // find the matching category based on the category title in the data
+      final tempCategory = categories.entries
+          .firstWhere((element) => element.value.title == i.value['Category'])
+          .value;
+
+      // create Items object and add it to the temporary list
+      templist.add(
+        Items(
+            name: i.value['name'],
+            category: tempCategory,
+            id: i.key,
+            quantity: i.value["Quantity"]),
+      );
+    }
+    setState(() {
+      itemsList = templist;
+    });
+  }
 
   void addItem() async {
-    final newItem = await Navigator.of(context).push<Items>(
+    await Navigator.of(context).push<Items>(
       MaterialPageRoute(
         builder: (context) {
           return const NewItem();
         },
       ),
     );
-    if (newItem == null) {
-      return;
-    }
-    setState(() {
-      itemsList.add(newItem);
-    });
+    loadItems();
   }
 
   void removeItem(Items item) {
